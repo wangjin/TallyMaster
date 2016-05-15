be1ca99f0642c0ac8b455fbb41ebd57426726dc4
王金 <18951603285@189.cn>
2016-05-15 12:39:19 +0800

    添加menu activity

---
+++ app/src/main/java/tk/jimmywang/tallymaster/activity/MenuActivity.java
@@ -0,0 +1,15 @@
+package tk.jimmywang.tallymaster.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import tk.jimmywang.tallymaster.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
