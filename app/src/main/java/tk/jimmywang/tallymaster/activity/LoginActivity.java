5cf87b182dae41222f06cdc96146e18560f2e36a
王金 <18951603285@189.cn>
2016-05-15 12:38:12 +0800

    添加login activity

---
+++ app/src/main/java/tk/jimmywang/tallymaster/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package tk.jimmywang.tallymaster.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tk.jimmywang.tallymaster.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
