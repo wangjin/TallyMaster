d61f97073c2808c66889a8dc78b3abe6fc0decf3
王金 <18951603285@189.cn>
2016-05-15 12:35:18 +0800

    添加volley requestqueue声明

---
+++ app/src/main/java/tk/jimmywang/tallymaster/application/TallyApplication.java
@@ -0,0 +1,17 @@
+package tk.jimmywang.tallymaster.application;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+
+/**
+ * Created with Android Studio by 王金 on 2016-05-15 下午12:31.
+ *
+ * @author 王金
+ * @version 1.0
+ */
+public class TallyApplication extends Application {
+
+    public static RequestQueue requestQueue;
+
+}
